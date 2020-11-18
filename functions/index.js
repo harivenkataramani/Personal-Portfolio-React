6ae1357eb272f9222841c155b6dc616a6cd40964
harivenkataramani <venkata.hari18@gmail.com>
2020-11-19 00:49:41 +0530

    image and contact form upload

---
+++ functions/index.js
@@ -0,0 +1,59 @@
+const functions = require("firebase-functions");
+const config = functions.config();
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+const cors = require("cors")({ origin: true });
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: config.user.email,
+    pass: config.user.pass,
+  },
+});
+
+let mailOptions = {
+  from: "hari.venkataramani18@gmail.com",
+};
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.contactFormMail = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    const { name, email, subject, message } = request.query;
+    mailOptions = {
+      from: "hari.venkataramani18@gmail.com",
+      to: "venkata.hari18@gmail.com",
+      subject: "Message Sent Successfully",
+      html: `
+                <p style="font-size:"16px">From: ${name}</p>
+                <p style="font-size:"16px">Email: ${email}</p>
+                <p style="font-size:"16px">Phone Number: ${subject}</p>
+                <p style="font-size:"16px">Message: ${message}</p>
+                `,
+    };
+    transporter.sendMail(mailOptions, (error) => {
+      if (error) {
+        response.send(error);
+      } else {
+        response.send("Mail Sent Successfully");
+      }
+    });
+
+    mailOptions = {
+      from: "hari.venkataramani18@gmail.com",
+      to: email,
+      subject: "We have received your message!",
+      html: `
+                  <p style="font-size:"16px">Hi ${name}, <br/>Thanks for contacting me, I will reach out to you very soon</p>
+                  <br/>
+                  <br/>
+                  <p style="font-size:"16px">Regards, <br/>Harivenkataramani. S</p>
+                  `,
+    };
+
+    transporter.sendMail(mailOptions);
+  });
+});
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Typewriter from "typewriter-effect";
 
 class Header extends Component {
   render() {
@@ -50,11 +51,6 @@ class Header extends Component {
               </a>
             </li>
             <li>
-              <a className="smoothscroll" href="#testimonials">
-                Testimonials
-              </a>
-            </li>
-            <li>
               <a className="smoothscroll" href="#contact">
                 Contact
               </a>
@@ -64,10 +60,16 @@ class Header extends Component {
 
         <div className="row banner">
           <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>
-              I'm a {city} based <span>{occupation}</span>. {description}.
-            </h3>
+            <h1 className="responsive-headline">
+              Hello, I'm <br />
+              <Typewriter
+                options={{
+                  strings: ["Harivenkataramani.", "Full Stack Web Developer"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </h1>
             <hr />
             <ul className="social">{networks}</ul>
           </div>
